fdb6788fb2f9629cef4d3c2ab3a72a8e2a11b2fd
satvir_sati <sssatvir12@gmail.com>
2024-08-15 17:23:53 +0530

    My Second Program in Java

---
+++ src/IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isRain=false;
+        if (isRain==true) {
+            System.out.println("Rain");
+        }
+        else{
+            System.out.println("Sunny...");
+        }
+    }
+}
