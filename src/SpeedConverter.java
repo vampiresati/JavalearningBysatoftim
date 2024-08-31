7b3a4e4941998084c4030bf2c6ac5634c43f9356
satvir_sati <sssatvir12@gmail.com>
2024-08-31 19:11:57 +0530

    Speedconverter

---
+++ src/SpeedConverter.java
@@ -0,0 +1,27 @@
+public class SpeedConverter {
+    // write code here
+    public static void main(String[] args) {
+
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+    public static long toMilesPerHour(double kilometersPerHour) {
+        long v;
+        if(kilometersPerHour<0){
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+    public static void printConversion(double kilometersPerHour) {
+        if(kilometersPerHour<0){
+            System.out.println("Invalid Value");
+            return ;
+        }
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+
+    }
+}
