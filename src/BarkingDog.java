6b53ef60bb5074c3101e92d1dca6d113197efe96
satvir_sati <sssatvir12@gmail.com>
2024-08-31 19:35:44 +0530

    barking code

---
+++ src/BarkingDog.java
@@ -0,0 +1,26 @@
+public class BarkingDog {
+    public static void main(String[] args) {
+        boolean barking;
+        barking=shouldWakeUp (true, 1);
+        System.out.println(barking);
+        barking=shouldWakeUp (false, 2);
+        System.out.println(barking);
+        barking=shouldWakeUp (true, 8);
+        System.out.println(barking);
+        barking=shouldWakeUp (true, -1);
+        System.out.println(barking);
+    }
+    public static boolean shouldWakeUp(boolean barking,int hourOfDay) {
+        if(hourOfDay<0 && hourOfDay>23){
+            return false;
+        }
+        if((barking && hourOfDay<8) || (barking && hourOfDay>22)){
+            return true;
+        }
+        return false;
+    }
+
+}
+
+
+
