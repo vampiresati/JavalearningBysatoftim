5d19192fc2e5aaa3dcf20f4bfa1302f07bc86616
satvir_sati <sssatvir12@gmail.com>
2024-08-31 20:04:04 +0530

    leap year program modification

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,32 +1,25 @@
 public class LeapYear {
-    public static void main(String[] args) {
-        boolean leapYear;
-        leapYear=isLeapYear(-1600);
-        System.out.println(leapYear);
-        leapYear=isLeapYear(1600);
-        System.out.println(leapYear);
-        leapYear=isLeapYear(2000);
-        System.out.println(leapYear);
-        leapYear=isLeapYear(2017);
-        System.out.println(leapYear);
 
-    }
     public static boolean isLeapYear(int year) {
-        if(year<=1||year==9999) {
+
+        // if year is less than 1 or greater than 9_999, it's invalid,
+        // so return false
+        if (year < 1 || year > 9999) {
             return false;
         }
-        if(year%4==0 && (year%100==0||year%400==0)){
-             {
-              return true;
+
+        if (year % 4 == 0) { // if year is divisible by 4, go to next step.
+            if (year % 100 != 0) { // if year is not divisible by 100, it's a leap year, so return true.
+                return true;
+            } else { // if year was divisible by 100, let's check if it's divisible by 400.
+                if (year % 400 == 0) { // if it's divisible by 400, it's leap year.
+                    return true;
+                } else { // otherwise, it's not a leap year, so return false.
+                    return false;
+                }
             }
+        } else { // year is not divisible by 4, so it's not a leap year.
+            return false;
         }
-        return false;
     }
-
-
 }
-
-
-
-
-
