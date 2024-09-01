64c088ae1df69d0cb75e2b978195ce429b2047aa
satvir_sati <sssatvir12@gmail.com>
2024-09-01 13:31:39 +0530

    ConvesionSecondformula vaildation

--- src/ConversionSeconds/ConvesionSecondformula.java
+++ src/ConversionSeconds/ConvesionSecondformula.java
@@ -3,9 +3,12 @@ package ConversionSeconds;
 public class ConvesionSecondformula {
     public static void main(String[] args) {
         System.out.println(convertToSeconds(3945));
-        System.out.println(convertToSeconds(65,45));
+        System.out.println(convertToSeconds(65,145));
     }
     public static String convertToSeconds(int seconds) {
+        if (seconds < 0) {
+            return "pass valid seconds";
+        }
         String hours;
         int hours1=seconds / 3600;
         int minutes1 = (seconds % 3600)/60;
@@ -14,9 +17,16 @@ public class ConvesionSecondformula {
         return hours;
     }
     public static String convertToSeconds(int hours, int minutes, int seconds) {
+
         return convertToSeconds(hours * 3600 + minutes * 60 + seconds);
     }
     public static String convertToSeconds(int minutes, int seconds) {
+        if (seconds < 0 || (!(seconds > 0 || seconds < 59))) {
+            return "pass valid seconds";
+        }
+        if (minutes < 0) {
+            return "pass valid mintues";
+        }
         return convertToSeconds(minutes * 60 + seconds);
     }
 
