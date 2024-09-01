b7ddaf6232fed27f0b9000ca2867a1415ff0590f
satvir_sati <sssatvir12@gmail.com>
2024-09-01 16:10:04 +0530

    ArrayFirstClass

---
+++ src/Arrayfunctionality/ArrayFirstClass.java
@@ -0,0 +1,11 @@
+package Arrayfunctionality;
+
+public class ArrayFirstClass {
+    public static void main(String[] args) {
+    int []numberArray={5,4,3,2,1};
+    for (int i=0;i<numberArray.length;i++){
+    System.out.println(numberArray[i]);
+    }
+    }
+
+}
