f179b4f3ed9a874769020baf31a8b8fc1f176639
satvir_sati <sssatvir12@gmail.com>
2024-09-01 16:36:19 +0530

    isCatPlaying

--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -10,22 +10,8 @@ public class PlayingCat {
     }
     public static boolean isCatPlaying(Boolean summer,int temperature) {
 
-        if (summer == true) {
-            if (temperature >=25 && temperature <=45) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else{
-            if (temperature >=25 && temperature <=35) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+        int max = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= max;
 
     }
 }
