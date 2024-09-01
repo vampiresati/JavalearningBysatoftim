e1484120c5614fcf19efb87eee0ea421eb096821
satvir_sati <sssatvir12@gmail.com>
2024-09-01 16:10:52 +0530

    ArrayFirstClass advanced loop

--- src/Arrayfunctionality/ArrayFirstClass.java
+++ src/Arrayfunctionality/ArrayFirstClass.java
@@ -3,8 +3,9 @@ package Arrayfunctionality;
 public class ArrayFirstClass {
     public static void main(String[] args) {
     int []numberArray={5,4,3,2,1};
-    for (int i=0;i<numberArray.length;i++){
-    System.out.println(numberArray[i]);
+
+    for(int n:numberArray){
+        System.out.println(n);
     }
     }
 
