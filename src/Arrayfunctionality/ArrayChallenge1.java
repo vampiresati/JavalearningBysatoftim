2270eb61a463a5117d8efe47c349d9b9e4583cc1
satvir_sati <sssatvir12@gmail.com>
2024-09-01 20:33:26 +0530

    Reverse Sorting array with input from user

--- src/Arrayfunctionality/ArrayChallenge1.java
+++ src/Arrayfunctionality/ArrayChallenge1.java
@@ -1,26 +1,37 @@
 package Arrayfunctionality;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Scanner;
 import java.util.Random;
 public class ArrayChallenge1 {
     public static void main(String[] args) {
-        int []n=new int[5];
+        int length=getIntegers();
+        int []n=new int[length];
         Random rand = new Random();
-        for(int j=0;j<5;j++){
+        for(int j=0;j<length;j++){
             n[j]=rand.nextInt(50);
         }
 
         System.out.println(Arrays.toString(n));
-
-        int []s=sortIntegers(n);
+        int []s=sortIntegers(n,length);
         System.out.println(Arrays.toString(s));
+        printArray(s);
     }
-    public static int[] sortIntegers(int []numbers){
-        int nn1[]=new int[5];
+    public static int getIntegers(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n=sc.nextInt();
+        return n;
+    }
+    public static void printArray(int[] arr){
+        for(int j=0;j<arr.length;j++){
+            System.out.println("Element "+ j+" contents " +arr[j]);
+        }
+    }
+    public static int[] sortIntegers(int []numbers,int length){
+        int nn1[]=new int[length];
         int temp;
         boolean flag=true;
         nn1=Arrays.copyOf(numbers,numbers.length);
-        System.out.println(Arrays.toString(nn1));
         while(flag){
             flag=false;
             for(int i=0;i<nn1.length-1;i++){
@@ -29,14 +40,9 @@ public class ArrayChallenge1 {
                     nn1[i]=nn1[i+1];
                     nn1[i+1]=temp;
                     flag=true;
-//                    System.out.print("----->");
-//                    System.out.println(Arrays.toString(nn1));
                 }
-//                System.out.print("---");
-//                System.out.println(Arrays.toString(nn1));
             }
         }
-//        System.out.println(Arrays.toString(nn1));
         return nn1;
     }
 
