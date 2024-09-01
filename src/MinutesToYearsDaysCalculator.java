af0d6fc142ffabdd8205a52e52b18559c86a5656
satvir_sati <sssatvir12@gmail.com>
2024-09-01 13:58:14 +0530

    MinutesToYearsDaysCalculator calculator

--- src/MinutesToYearsDaysCalculator.java
+++ src/MinutesToYearsDaysCalculator.java
@@ -1,20 +1,23 @@
 public class MinutesToYearsDaysCalculator {
+
+    private static final int MIN_PER_HOUR = 60;
+    private static final int HOURS_PER_DAY = 24;
+    private static final int DAYS_PER_YEAR = 365;
+    private static final int MIN_PER_DAY = HOURS_PER_DAY * MIN_PER_HOUR;
+    private static final int MIN_PER_YEAR = MIN_PER_HOUR * HOURS_PER_DAY * DAYS_PER_YEAR;
     public static void main(String[] args) {
         printYearsAndDays(525600);
         printYearsAndDays(1051200);
         printYearsAndDays(561600);
     }
-    public static void printYearsAndDays(long minutes){
-        if(minutes <0){
+    public static void printYearsAndDays(long minutes) {
+
+        if (minutes < 0) {
             System.out.println("Invalid Value");
+            return;
         }
-        int hours=(int)minutes/60;
-        int day=hours/24;
-        int year=day/365;
-
-        System.out.println(year+"year "+day%365+"days "+hours%(24)+"hours ");
-
-
-
+        long years = minutes / MIN_PER_YEAR;
+        long days = (minutes / MIN_PER_DAY) % DAYS_PER_YEAR;
+        System.out.println(minutes + " min = " + years + " y and " + days + " d");
     }
 }
