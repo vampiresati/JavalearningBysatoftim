a975f5299dc097d8fd6ff8d60c8d192ed8270fc0
satvir_sati <sssatvir12@gmail.com>
2024-09-07 11:39:55 +0530

    ReverseArraychallege challenge another way

--- src/Arrayfunctionality/ReverseArraychallege.java
+++ src/Arrayfunctionality/ReverseArraychallege.java
@@ -1,19 +1,74 @@
 package Arrayfunctionality;
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReverseArraychallege {
+
     public static void main(String[] args) {
-        int[] a={1,2,3,4,5};
-        System.out.println(Arrays.toString(a));
-        int[] b=reverseArray(a);
-        System.out.println(Arrays.toString(b));
+
+        int[] returnedArray = readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
+
+//        int returnedMin = findMin(returnedArray);
+//        System.out.println("min = " + returnedMin);
+
+//        reverse(returnedArray);
+//        System.out.println("Final: " + Arrays.toString(returnedArray));
+
+        int[] reversedCopy = reverseCopy(returnedArray);
+        System.out.println("After reverse " + Arrays.toString(returnedArray));
+        System.out.println("reversedCopy " + Arrays.toString(reversedCopy));
     }
-    public static int[] reverseArray(int[] arr) {
-        int[] arrCopy = new int[arr.length];
-        //arrCopy=Arrays.copyOf(arrCopy, arr.length);
-        boolean flag=true;
-        for(int i=0;i<arr.length;i++){
-            arrCopy[arr.length-i-1]=arr[i];
+
+    private static int[] readIntegers() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a list of integers, separated by commas:");
+        String input = scanner.nextLine();
+
+        String[] splits = input.split(",");
+        int[] values = new int[splits.length];
+
+        for (int i = 0; i < splits.length; i++) {
+            values[i] = Integer.parseInt(splits[i].trim());
         }
-        return arrCopy;
+
+        return values;
+    }
+
+    private static int findMin(int[] array) {
+
+        int min = Integer.MAX_VALUE;
+        for (int el : array) {
+            if (el < min) {
+                min = el;
+            }
+        }
+
+        return min;
+    }
+
+    private static void reverse(int[] array) {
+
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+            System.out.println("--> " + Arrays.toString(array));
+        }
+    }
+
+    private static int[] reverseCopy(int[] array) {
+
+        int[] reversedArray = new int[array.length];
+        int maxIndex = array.length - 1;
+        for (int el : array) {
+            reversedArray[maxIndex--] = el;
+        }
+
+        return reversedArray;
     }
 }
