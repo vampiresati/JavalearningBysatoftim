4662f44b4e5d8152efd20bf0119e00ccfd2716cb
satvir_sati <sssatvir12@gmail.com>
2024-09-07 12:42:05 +0530

    Array list functionality

---
+++ src/Listfunctionality/Listfirstprogram.java
@@ -0,0 +1,21 @@
+package Listfunctionality;
+
+import java.util.ArrayList;
+
+public class Listfirstprogram {
+    public static void main(String[] args) {
+        ArrayList list=new ArrayList();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        ArrayList<Integer> intlist=new ArrayList<>();
+        intlist.add(1);
+        intlist.add(2);
+        System.out.println(intlist);
+
+    }
+
+
+}
+
